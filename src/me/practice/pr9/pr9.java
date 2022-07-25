029b0772e21b42b6cc7821e7424a10613a464c88
Mikhail <fedykov0@yandex.ru>
2022-07-25 20:12:57 +0300

    Added a new method because of the condition

--- src/me/practice/pr9/pr9.java
+++ src/me/practice/pr9/pr9.java
@@ -11,21 +11,10 @@ public class Pr9 {
         Pr9 mainObject = new Pr9();
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        int sumDigits = 0;
         Map<Integer, String> fruits = new HashMap<Integer, String>();
 
         mainObject.createHashMap(fruits);
-
-        //Используем цикл do while из-за условия задачи:
-        //в любом случае нужно будет пересчитывать заданное число, а затем искать его в списке.
-        //Далее высчитываем новое число, основываясь на цифрах, из которых состояло старое число.
-        do {
-            sumDigits = number / 100 + number / 10 % 10 + number % 10;
-            System.out.println("Сумма цифр числа = " + sumDigits);
-            number = number - sumDigits;
-            System.out.println("Новое число = " + number);
-        } while (!fruits.containsKey(number));
-        System.out.println("Фрукт, который подходит по получившемуся числу: " + fruits.get(number));
+        mainObject.findFruit(fruits, number);
     }
 
     //Метод заполняет хеш-мапу значениями из файла.
@@ -39,13 +28,27 @@ public class Pr9 {
                 myMap.put(key, value);
             }
             fr.close();
-        }
-        catch (IOException exception){
+        } catch (IOException exception) {
             exception.getMessage();
         }
-        if (myMap.isEmpty()){
+        if (myMap.isEmpty()) {
             System.out.println("Похоже коллекция пуста");
         }
         return myMap;
+    }
+
+    //Метод получает число от пользователя и определяет соответствующий фрукт
+    public void findFruit(Map<Integer, String> myMap, int numberFromUser) {
+        int sumDigits = 0;
+        //Используем цикл do while из-за условия задачи:
+        //в любом случае нужно будет пересчитывать заданное число, а затем искать его в списке.
+        //Далее высчитываем новое число, основываясь на цифрах, из которых состояло старое число.
+        do {
+            sumDigits = numberFromUser / 100 + numberFromUser / 10 % 10 + numberFromUser % 10;
+            System.out.println("Сумма цифр числа = " + sumDigits);
+            numberFromUser = numberFromUser - sumDigits;
+            System.out.println("Новое число = " + numberFromUser);
+        } while (!myMap.containsKey(numberFromUser));
+        System.out.println("Фрукт, который подходит по получившемуся числу: " + myMap.get(numberFromUser));
     }
 }
